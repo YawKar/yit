0723ae7b241e6f518419477fbb4cdf7402bf8336
yawkar <yawkarpov@gmail.com>
2023-03-25 12:08:36 +0300

    refactor: add namespaces for better reading

--- src/yit/actions/init/InitAction.cpp
+++ src/yit/actions/init/InitAction.cpp
@@ -5,28 +5,33 @@
 
 namespace fs = boost::filesystem;
 
-void yit::actions::InitAction::init_repository(std::string path) {
-    fs::path path_obj(path);
-    if (fs::is_regular_file(path)) {
-        throw std::runtime_error("Couldn't initialize repository because path is a regular file");
-        return;
-    }
-    if (!fs::exists(path_obj)) {
-        std::cout << "Warning: path didn't exist. Yit creates it." << std::endl;
-        fs::create_directories(path_obj);
-    }
-    auto yit_folder = path_obj / ".yit";
-    if (fs::exists(yit_folder)) {
-        throw std::runtime_error(".yit folder already exists. If you want to reinitialize repository, perhaps, delete the existing .yit folder.");
-    }
-    fs::create_directory(yit_folder);
-    auto objects_folder = path_obj / "objects";
-    fs::create_directory(objects_folder);
-    auto refs_folder = path_obj / "refs";
-    fs::create_directory(refs_folder);
-    auto head_ref_file = path_obj / "HEAD";
-    fs::ofstream head_ref(head_ref_file);
-    head_ref << "ref: refs/heads/master\n";
+namespace yit {
+    namespace actions {
 
-    std::cout << "Initialized empty Yit repository in " << yit_folder << std::endl;
+        void InitAction::init_repository(std::string path) {
+            fs::path path_obj(path);
+            if (fs::is_regular_file(path)) {
+                throw std::runtime_error("Couldn't initialize repository because path is a regular file");
+                return;
+            }
+            if (!fs::exists(path_obj)) {
+                std::cout << "Warning: path didn't exist. Yit creates it." << std::endl;
+                fs::create_directories(path_obj);
+            }
+            auto yit_folder = path_obj / ".yit";
+            if (fs::exists(yit_folder)) {
+                throw std::runtime_error(".yit folder already exists. If you want to reinitialize repository, perhaps, delete the existing .yit folder.");
+            }
+            fs::create_directory(yit_folder);
+            auto objects_folder = path_obj / "objects";
+            fs::create_directory(objects_folder);
+            auto refs_folder = path_obj / "refs";
+            fs::create_directory(refs_folder);
+            auto head_ref_file = path_obj / "HEAD";
+            fs::ofstream head_ref(head_ref_file);
+            head_ref << "ref: refs/heads/master\n";
+
+            std::cout << "Initialized empty Yit repository in " << yit_folder << std::endl;
+        }
+    }
 }
